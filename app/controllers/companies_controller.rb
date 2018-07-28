00ae501007218115972e495cec4aaca6640bf13b
Reayah Bravo <reayahbravo@gmail.com>
2018-07-28 12:43:14 -0700

    WIP working on the company show view

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,7 @@
 class CompaniesController < ApplicationController
 
     def index
+        @companies = Company.all
     end
 
     def new
@@ -13,9 +14,13 @@ class CompaniesController < ApplicationController
     end
 
     def show
+        # @company = Company.find_by_user_id(current_user.id)
+        @company = Company.find_by_owner_id(User.last.id)
+        # @company_user_id = current_user.id
+
     end
 
     def edit
     end
-    
+
 end
