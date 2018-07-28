f7d10aa7caacbfd272842f5454f44cf1ab50e50a
Iana Markevitch <iana.markevitch@gmail.com>
2018-07-28 11:16:14 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   
   resources :companies 
-  
-
-  # get('/', { to: 'company#index', as: 'home' })
-    get('/', { to: 'user#new', as: 'home' })
 end
