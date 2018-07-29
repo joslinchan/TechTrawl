2ef342484e3cbd08e6bbcacac777df59a0880ca8
sarithapolisetty <sarithapolisetty14@gmail.com>
2018-07-28 19:12:32 -0700

    NewsController is working

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,9 +5,12 @@ class NewsController < ApplicationController
         newsapi = News.new("7cc1435c50a14ace808ab284ec562f9c")
 
         @top_headlines = newsapi.get_top_headlines(
-            q: 'business'
-           
+            q: 'startup'
+            
         )
-        
+
+        #    render json: @top_headlines
     end
+
+       
 end
