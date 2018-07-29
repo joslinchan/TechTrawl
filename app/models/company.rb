81b27b19f00a073099a980c000a63515044dc470
Iana Markevitch <iana.markevitch@gmail.com>
2018-07-29 16:13:05 -0700

    Add events & articles to company show

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,14 +3,13 @@ class Company < ApplicationRecord
     has_one :users, dependent: :nullify
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
-    has_many :events, dependent: :destroy
-    has_many :articles, dependent: :destroy
+    has_many :events, dependent: :nullify
+    has_many :articles, dependent: :nullify
 
     validates(:name, presence: true, uniqueness: true)
 
     #scope :tech_team, -> (number) { where("tech_team >= 10", number }
     #the above code is Jocelyn trying to apply a search filter
-
 
     def self.search(term)
         @companies = 
